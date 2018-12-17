d7c5598ba6a2544b0cdde72a08e834f2667cb350
Pierre-Yves Dupont <pierre.dupont@esr.cri.nz>
2018-12-17 14:46:47 +1300

    Python 3 version. Uses Click for the command line and ezpq for the process queueing.

--- launch_parallel.py
+++ launch_parallel.py
@@ -5,92 +5,123 @@ Created on Aug 11, 2017
 '''
 
 
-import multiprocessing
+import ezpq
+import subprocess
+import click
+import shlex
 import os
 import sys
 import time
 import datetime
-from commands import getoutput
+import multiprocessing
 
-def run(x):
+def run(t):
     """
     Run the command
     """
-    return os.system(x)
+    if "@echo" in t:
+        if t.startswith('#'): t = t[1:]
+        line = " ".join([x for x in t.split() if not x.startswith("@")])
+        if "@time" in t:
+            line += " `date`"
+        t = "echo \"%s\r\n\"" % line
+    elif "@time" in t:
+        t = "echo `date`\r\n"
+    output = subprocess.check_output(t, shell=True).decode('utf-8')
+    if len(output.strip()):
+        sys.stderr.write(output)
+
+def print_sizes(Q):
+    msg = 'Total: {0}; Waiting: {1}; Working: {2}; Completed: {3}'.format(
+        Q.size(),
+        Q.size(waiting=True),
+        Q.size(working=True),
+        Q.size(completed=True)
+    )
+    print(msg)
 
 def parallel_run(t, nb_cores):
     """
     Runs a list of commands in parallel using a given number of processes
     """
-    sys.stdout.write("%s commands to execute on %s cores\n" %(len(t), nb_cores))    
-    for i,tt in enumerate(t):
-        if "@echo" in tt:
-            if tt.startswith('#'): tt = tt[1:]
-            line = " ".join([x for x in tt.split() if not x.startswith("@")])
-            if "@time" in tt:
-                line += " `date`"
-            t[i] = "echo \"%s\r\n\"" % line
-        elif "@time" in tt:
-            t[i] = "echo `date`\r\n"
-    num_tasks = len(t)
-    p = multiprocessing.Pool(processes=nb_cores)
-    rs = p.imap_unordered(run, t)
-    p.close()
-    while (True):
-      completed = rs._index
-      if (completed == num_tasks): break
-      sys.stderr.write("Waiting for %s tasks to complete...\r" % (num_tasks-completed))
-      time.sleep(1)
+    with ezpq.Queue(n_workers=nb_cores) as Q:
+        # enqueue jobs
+        for tt in t:
+            Q.put(run, args=[tt])
 
-def main():
-    NB_ARGS = 2
-    """
-    I dont use argument parsing libraries here to be compatible with python 2.6 and 2.7
-    """
-    if len(sys.argv) != NB_ARGS + 1:
-        sys.stderr.write("Give two parameters: input command file and number of cores to use\n")
-        exit(2)
+        # repeatedly wait 1 second for jobs to finish until n_remaining == 0.
+        n_remaining = Q.wait(timeout=2)
+        while n_remaining > 0:
+            print('{0:5} jobs remain.'.format(n_remaining))
+            n_remaining = Q.wait(timeout=2)
+        print('{0:5} jobs remain.'.format(n_remaining))
 
-    infile, nb_cores = sys.argv[1:]
+def read_input(infile):
+    """Create the list of commands. In case of blocks: it is a list of lists with one list of commands per blocks
+    
+    :param infile: Input file
+    :type infile: file
+
+    :return: tasks list
+    :rtype: list
+    """
+    t = []
+    blocks = False
+    lines = infile.readlines()
+    if "@block" in "\n".join(lines):
+        blocks = True
+    if blocks:
+        tt = []
+        for line in lines:
+            if "@block" in line:
+                if tt:
+                    t.append(tt)
+                tt = ["#@echo @time %s" % line[line.index("@block") + len("@block"):].strip()]
+            else:
+                if "#@exit" in line: break #usefull for debug
+                if line.startswith('#') and "@" not in line: continue
+                if not line.strip(): continue
+                tt.append(line.strip())
+        t.append(tt)
+    else:
+        for line in lines:
+            if "#@exit" in line: break #usefull for debug
+            if line.startswith('#') and "@" not in line: continue
+            if not line.strip(): continue
+            t.append(line.strip())
+    return t, blocks
+
+def process(t, blocks, nb_cores):
+    #In case of blocks, many parallel_runs are started sequencially
+    if blocks:
+        cmds = []       
+        for tt in t:
+            cmds.append(sum([1 for x in tt if(not(x.startswith("@")) and not(x.startswith("#")))]))
+        sys.stderr.write("%s command blocks containing %s commands to run in total\n" % (len(t), sum(cmds)))
+        for tt in t:
+            parallel_run(tt, nb_cores)
+    else:
+        parallel_run(t, nb_cores)
+
+@click.command()
+@click.option("-t", "nb_cores", help="Number of threads to use", default=1, type=int)
+@click.argument("infile", required=True, type=click.File('r'))
+def main(nb_cores, infile):
+    """This python utility helps starting tasks on many cores for parallel processing.
+    \b
+    Example:
+        launch_parallel -t 24 commands_test1.sh
+    """
     try:
-        nb_cores = int(nb_cores)
         if nb_cores > multiprocessing.cpu_count(): raise ValueError()
         if nb_cores <= 0: raise ValueError()
     except ValueError:
         sys.stderr.write("%s is not a valid core number (should be an integer between 0 and %s)\n" % (nb_cores, multiprocessing.cpu_count()))
         exit(3)
 
-    if not os.path.isfile(infile):
-        sys.stderr.write("%s not a valid file\n" % infile)
-        exit(3)
+    t, blocks = read_input(infile)
+    process(t, blocks, nb_cores)
 
-    #Create the list of commands. In case of blocks: it is a list of lists with one list of commands per blocks
-    t = []
-    blocks = False
-    with open(infile) as f:
-        for line in f:
-            if "@block" in line:
-                t.append("@echo @time %s\n" % line[line.index("@block") + len("@block"):].strip())
-                if not blocks:
-                    t = [list(t)]
-                    blocks = True
-                t.append([])
-                continue
-            if "#@exit" in line: break #usefull for debug
-            if line.startswith('#') and "@" not in line: continue
-            if not line.strip(): continue
-            if blocks:
-                t[-1].append(line.strip())
-            else:
-                t.append(line.strip())
-                
-    #In case of blocks, many parallel_runs are started sequencially
-    if blocks:
-        sys.stderr.write("%s command blocks containing %s commands to run in total\n" % (len(t), sum([len(x) for x in t])))
-        for tt in t:
-            parallel_run(tt, nb_cores)
-    else:
-        parallel_run(t, nb_cores)
 
 if __name__ == "__main__":
     main()
